8c156f8782584dc0cae6cf5502888fca32fd2198
Adrian Czok <qentinios@gmail.com>
2019-06-01 13:32:44 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Sitemap generator
+With this tool you can generate sitemap for any url. 
+
+Copy and paste link to the form, select recursion depth and output format.
+
+Script should generate sitemap tree, keep in mind that generation time depends on selected options and site complexity.
---
+++ test_app.py
@@ -0,0 +1,15 @@
+import unittest
+
+from sitemap_generator import app
+
+
+class AppTests(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    def test_home(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
