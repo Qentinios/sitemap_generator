5b26395b6fc09ab26ba0cd5a0f9261438227de6b
Adrian Czok <qentinios@gmail.com>
2019-06-22 23:59:27 +0200

    Added xml and text output format

--- sitemap_generator.py
+++ sitemap_generator.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, jsonify, make_response
 import os
 from forms import GeneratorForm
 from generator import Generator
+from lxml import etree
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
@@ -15,13 +16,33 @@ def home():
     form = GeneratorForm()
 
     if form.validate_on_submit():
-        generator = Generator(url=form.url.data, depth=int(form.depth.data))
+        generator = Generator(url=form.url.data, depth=int(form.depth.data), flat=(form.format.data != 'screen'))
         sitemap = generator.generate()
 
         if form.format.data == 'screen':
             return make_response(jsonify(sitemap), 200)
+        elif form.format.data == 'xml':
+            root = etree.Element('sitemap')
+            home_url = etree.Element('url')
+            home_url.text = form.url.data
+            root.append(home_url)
+
+            for hyperlink in sitemap:
+                url = etree.Element('url')
+                url.text = hyperlink
+                root.append(url)
+
+            xml = etree.tostring(root, pretty_print=True)
+
+            response = make_response(xml, 200)
+            response.headers['Content-Type'] = 'application/xml'
+
+            return response
         else:
-            return 'file'
+            text = form.url.data + '<br>'
+            text += '<br>'.join(sitemap)
+
+            return make_response(text, 200)
     else:
         return render_template('index.html', form=form)
 
